672d176bd28a9bc980764af9c484a8b449a69eed
Intuitive Harmony <j.horst77@gmail.com>
2023-06-06 17:47:55 -0600

    start handleGenerateGroups()

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ const students = [
   "Anish",
 ];
 
+// Generate Groups
+const handleGenerateGroups = () => {
+  console.log(students);
+};
+
 $(() => {
   // Loop that prints list of students to the DOM
   for (let i = 0; i < students.length; i++) {
@@ -24,4 +29,7 @@ $(() => {
     const $studentLi = $(`<li>`).text(students[i]);
     $("#studentList").append($studentLi);
   }
+
+  // Grab the generate button
+  $("#generateBtn").on("click", handleGenerateGroups);
 });
